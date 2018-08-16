8e6d3689630f5fec7f7d8bb8d7f560bbeab75f7a
Takuya Nakajima <airu85@live.jp>
2018-08-16 12:40:27 +0900

    addex circle.yum

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push willrstern/sample-node
+docker push hitaraku/sample-node
 
-ssh deploy@159.203.127.59 << EOF
-docker pull willrstern/sample-node:latest
+ssh deploy@159.65.10.100 << EOF
+docker pull hitaraku/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi willrstern/sample-node:current || true
-docker tag willrstern/sample-node:latest willrstern/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 willrstern/sample-node:current
+docker rmi hitaraku/sample-node:current || true
+docker tag hitaraku/sample-node:latest hitaraku/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 hitaraku/sample-node:current
 EOF
